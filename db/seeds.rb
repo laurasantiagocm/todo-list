07d6d8ab9a8535cf5f35ec81e8fa92c3130bd37f
Laura Santiago Campos Oliveira <laaaaaaa45@gmail.com>
2020-12-01 09:40:37 -0300

    Validações e seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+List.create(
+  name: 'Lista criada com o seed'
+)
+
+10.times do |i|
+  List.create(
+    name: "Lista #{i}"
+  )
+end
+
+10.times do
+  List.create(
+    name: Faker::Lorem.paragraph
+  )
+end
+
+20.times do
+  Todo.create(
+    name: Faker::Lorem.paragraph,
+    completed: [true, false].sample,
+    list_id: rand(1..List.count)
+  )
+end
